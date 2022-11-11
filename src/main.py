3d063336173b534d40b52effc8b82d6c62a25000
davimc <davimatosc@hotmail.com>
2022-11-11 17:47:33 -0300

    fix

--- src/main.py
+++ src/main.py
@@ -11,21 +11,26 @@ def fill_gsheet():
     gsheets.populate_sheet(day)
     
 def set_file_id():
-    new_id = input('Informe o id para a planilha do novo mês: ')
-    file_handler.set_gsheet_id(new_id)
-    print('Nova planilha cadastrada')
+    try:
+        new_id = input('Informe o id para a planilha do novo mês: ')
+        file_handler.set_gsheet_id(new_id)
+        print('Nova planilha cadastrada')
+    except Exception as error:
+        print(err)
     
 def menu():
-    while True:
-        choice = input(f'1.Popular planilha\n' +
-                 '2.Alterar id da planilha\n')
-        match choice:
-            case '1':
-                fill_gsheet()
-            case '2': 
-                set_file_id()
-            case _:
-                print('Opção não identificada')
+    
+    choice = input(f'1.Popular planilha\n' +
+                '2.Alterar id da planilha\n')
+    match choice:
+        case '1':
+            fill_gsheet()
+        case '2': 
+            set_file_id()
+            menu()
+        case _:
+            print('Opção não identificada')
+            menu()
             
 if __name__ == '__main__':
     menu()
